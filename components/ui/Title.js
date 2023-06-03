81564658b72f287c115598112c83efe89974a6da
in-kim <ehddls960617@naver.com>
2023-06-03 23:38:34 +0900

    Update. Platform api 사용하여 os 별 다르게 스타일링

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,5 +1,4 @@
-import {Text, StyleSheet} from 'react-native';
-import Color from "../../constans/color";
+import {Text, StyleSheet, Platform} from 'react-native';
 export default function Title({children}){
   return (
     <Text style={styles.title}>{children}</Text>
@@ -12,8 +11,9 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: 'white',
     textAlign: 'center',
-    borderWidth: 2,
+    borderWidth: Platform.select({ ios: 0, android: 2}),
     borderColor: 'white',
     padding: 12,
+    maxWidth: '80%',
   }
 })
